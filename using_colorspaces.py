31cc0da2102c5d97b20897bdcc7e2db4f76b9bc4
Avadhoot S <abshej@users.noreply.github.com>
2017-11-20 22:14:34 +0530

    Update using_colorspaces.py

--- using_colorspaces.py
+++ using_colorspaces.py
@@ -11,25 +11,24 @@ import matplotlib.pyplot as plt
 import sys 
 
 frame = cv2.cvtColor(cv2.imread(str(sys.argv[1]), -1), cv2.COLOR_BGR2RGB) #standard bgr converted to rgb beforehand
-#framegr = cv2.imread(str(sys.argv[1]), 0) #grayscale frame
 
 #median blurring frame
 #frame = cv2.medianBlur(frame, 3)
     
 #ycbcr thresholding
-framey = cv2.cvtColor(frame, cv2.COLOR_RGB2YCR_CB) #rgb to ycbcr conversion
+framey = cv2.cvtColor(frame, cv2.COLOR_RGB2YCR_CB) #rgb to ycrcb conversion
 
 lower_y = np.array([230,120,60])
 upper_y = np.array([300,300,150])
 thres_mask_y1 = cv2.inRange(framey, lower_y, upper_y) #obtaining binary
-bit_mask_y1 = cv2.bitwise_and(framey, framey, mask= thres_mask_y1) 
+#bit_mask_y1 = cv2.bitwise_and(framey, framey, mask= thres_mask_y1) 
 
 lower_y2 = np.array([130,180,30])
-upper_y2 = np.array([230,200,80])
+upper_y2 = np.array([230,200,90])
 thres_mask_y2 = cv2.inRange(framey, lower_y2, upper_y2) #obtaining binary
 #bit_mask_y2 = cv2.bitwise_and(framey, framey, mask= thres_mask_y2)
 
-thres_mask_y = cv2.bitwise_or(thres_mask_y1, thres_mask_y2) #combining both results 
+thres_mask_y = cv2.bitwise_or(thres_mask_y1, thres_mask_y2) 
 
 #obtaining masked image
 
@@ -44,8 +43,7 @@ upper_red2 = np.array([240,120,80])
 thres_mask_red2 = cv2.inRange(frame, lower_red2, upper_red2)
 #bit_mask_rgb2 = cv2.bitwise_and(frame, frame, mask= thres_mask_red)
 
-thres_mask_red = cv2.bitwise_or(thres_mask_red1, thres_mask_red2) #combining both results
-
+thres_mask_red = cv2.bitwise_or(thres_mask_red1, thres_mask_red2)
 
 #hsv thresholding
 hsv_edit = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV).astype(np.uint8) 
@@ -55,7 +53,7 @@ upper_blue = np.array([70,300,300])
 thres_mask_hsv = cv2.inRange(hsv_edit, lower_blue, upper_blue)
 #bit_mask_hsv = cv2.bitwise_and(frame,frame, mask= thres_mask)
 
-#bitwise condition
+#mask
 thres_mask_hsvnrgb = cv2.bitwise_or(thres_mask_hsv, thres_mask_red)
 bit_mask_three = cv2.bitwise_and(thres_mask_hsvnrgb, thres_mask_y)
 
@@ -67,9 +65,10 @@ if float(n)/float(frame.size) > 0.0005: #need to change
 
 #displaying results
 plt.subplot(121), plt.imshow(frame)
-plt.title('original frame'), plt.xticks([]), plt.yticks([])
+plt.title('frame'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(bit_mask_three)
-plt.title('resulting frame'), plt.xticks([]), plt.yticks([])
+plt.title('res_mix'), plt.xticks([]), plt.yticks([])
+
 if c==1:
     plt.figtext(.02, .02, "FLAME DETECTED\n", style='italic', bbox={'facecolor':'red', 'alpha':0.5, 'pad':10})
 else:
